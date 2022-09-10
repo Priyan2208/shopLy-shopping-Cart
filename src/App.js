08a739cee32e6c4b33d26eff039d268985ee7160
Priyan2208 <108563674+Priyan2208@users.noreply.github.com>
2022-09-10 12:31:14 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useThemeHook } from './GlobalComponents/ThemeProvider';
-import Header from './components/Header';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useThemeHook } from "./GlobalComponents/ThemeProvider";
+import Header from "./components/Header";
 import { Router } from "@reach/router";
 
 //Pages
-import Home from './Pages/Home';
-import Cart from './Pages/Cart';
-
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
 
 function App() {
   const [theme] = useThemeHook();
   return (
-    <main className={theme? 'bg-black': 'bg-light-2'} style={{ height: '100vh', overflowY: 'auto'}}>
-      <Header/>
+    <main
+      className={theme ? "bg-black" : "bg-light-2"}
+      style={{ height: "100vh", overflowY: "auto" }}
+    >
+      <Header />
       <Router>
         <Home path="/" />
         <Cart path="/cart" />
